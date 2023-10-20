87174da6391b172e31e008b78ee96acfde1d420b
Michele Selvaggio <m.selvaggio@matrixconsulting.it>
2023-10-20 12:24:02 +0200

    altro aggiornamento

--- src/integration/bigcommerce/lib/fetch.js
+++ src/integration/bigcommerce/lib/fetch.js
@@ -1,9 +1,9 @@
 export async function graphQlFetch( {query, variables, cache = 'force-cache'} ){
-    const BARER_TOKEN = "eyJ0eXAiOiJKV1QiLCJhbGciOiJFUzI1NiJ9.eyJjaWQiOjEsImNvcnMiOlsiaHR0cHM6Ly9zdG9yZS12aGRkOW52OGVmLm15YmlnY29tbWVyY2UuY29tIl0sImVhdCI6MTg4NTYzNTE3NiwiaWF0IjoxNjk1Mzc0MTU1LCJpc3MiOiJCQyIsInNpZCI6MTAwMjk2NDg2Mywic3ViIjoidHE2dDhkYXRpaTdsMDJkb2xrM3Q0MjR3emQ4c2k5cSIsInN1Yl90eXBlIjoyLCJ0b2tlbl90eXBlIjoxfQ.mMEtmdQYM5sFvfNeA8r1nvB38vAtxTrLP2RfZw-aCGm_iTpb2Yb38qfQAsjgISHaUsTgX-kZpJY2S-K-hgvbyQ";
+    const BARER_TOKEN = "eyJ0eXAiOiJKV1QiLCJhbGciOiJFUzI1NiJ9.eyJjaWQiOjEsImNvcnMiOlsiaHR0cDovL2xvY2FsaG9zdDozMDAwIiwiaHR0cHM6Ly9iaWdjb21tZXJjZS1jYXRlZ29yeS52ZXJjZWwuYXBwIl0sImVhdCI6MTg4NTYzNTE3NiwiaWF0IjoxNjk3Nzk2ODg4LCJpc3MiOiJCQyIsInNpZCI6MTAwMjk2NDg2Mywic3ViIjoidHE2dDhkYXRpaTdsMDJkb2xrM3Q0MjR3emQ4c2k5cSIsInN1Yl90eXBlIjoyLCJ0b2tlbl90eXBlIjoxfQ.Hwpu7HgsJWRPWbjeGi8bxaE93V4YXF4B41EqkPci1Gqpm1rphupNANpr4DVSo9zKpJxTdoWVKY2-MYV1mCbu6g";
   //const endpoint = "https://ms-sandbox.mybigcommerce.com/graphql";
   const endpoint = "https://store-vhdd9nv8ef.mybigcommerce.com/graphql";
 
-  
+
   const reinvalidate = (cache == 'force-cache') ? {} : { revalidate: 900 }  // 15 minutes
 
   const result = await fetch(`${endpoint}`, {
