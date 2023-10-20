84979a7dbd10200640888c9e3072b795e054af49
Michele Selvaggio <m.selvaggio@matrixconsulting.it>
2023-10-20 15:45:16 +0200

    mod

--- src/integration/bigcommerce/lib/fetch.js
+++ src/integration/bigcommerce/lib/fetch.js
@@ -2,8 +2,8 @@ export async function graphQlFetch( {query, variables, cache = 'force-cache'} ){
   const BARER_TOKEN =  process.env.BIGCOMMERCE_BARER_TOKEN;
   const endpoint = process.env.BIGCOMMERCE_ENDPOINT;
 
-  //const reinvalidate = (cache == 'force-cache') ? {} : { revalidate: 900 }  // 15 minutes
-  const reinvalidate = 'force-cache';
+  const reinvalidate = (cache == 'force-cache') ? {} : { revalidate: 900 }  // 15 minutes
+  //const reinvalidate = 'force-cache';
 
   const result = await fetch(`${endpoint}`, {
     method: "POST",
